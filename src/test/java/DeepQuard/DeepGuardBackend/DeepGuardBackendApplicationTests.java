dce34b04d0ffe291d7305a86551ee03e8c152576
OppongNicholas303 <nicholas.oppong@amalitech.com>
2025-09-15 20:16:20 +0000

    Initial commit

---
+++ src/test/java/DeepQuard/DeepGuardBackend/DeepGuardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package DeepQuard.DeepGuardBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepGuardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
